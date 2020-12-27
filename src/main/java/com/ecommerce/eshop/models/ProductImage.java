1864672f55d5beabe9b805432a76512d4ddc8d9a
StanislawNagorski <stanislaw.nagorski@gmail.com>
2020-12-27 18:37:44 +0100

    product and productImage models added

---
+++ src/main/java/com/ecommerce/eshop/models/ProductImage.java
@@ -0,0 +1,24 @@
+package com.ecommerce.eshop.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long imageId;
+    private Long productId;
+    private String imageUrl;
+
+}
