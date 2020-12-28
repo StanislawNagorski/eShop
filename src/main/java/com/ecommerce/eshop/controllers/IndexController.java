8ffdd239ccbc57751e1215c27cf528198aa0f906
StanislawNagorski <stanislaw.nagorski@gmail.com>
2020-12-28 18:21:05 +0100

    "" get mapping added

---
+++ src/main/java/com/ecommerce/eshop/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.ecommerce.eshop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class IndexController {
+
+    @GetMapping
+    public String apiIntro(){
+        return "Intro to API";
+    }
+}
