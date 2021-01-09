e0a2ee7203794abd2684111fae9ea969498ed2ae
StanislawNagorski <stanislaw.nagorski@gmail.com>
2021-01-09 11:11:16 +0100

    order model created

---
+++ src/main/java/com/ecommerce/eshop/order/models/Order.java
@@ -0,0 +1,30 @@
+package com.ecommerce.eshop.order.models;
+
+import com.ecommerce.eshop.product.models.Product;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToMany
+    private List<Product> products;
+    private BigDecimal totalAmount;
+    private Integer totalQuantity;
+    @CreationTimestamp
+    private LocalDateTime creationTime;
+    @UpdateTimestamp
+    private LocalDateTime updateTime;
+}
