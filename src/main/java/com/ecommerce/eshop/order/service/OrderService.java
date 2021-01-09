99670a9c45ceda8585fccd8159fd0a26b52f7024
StanislawNagorski <stanislaw.nagorski@gmail.com>
2021-01-09 12:01:05 +0100

    tests without implementation added

--- src/main/java/com/ecommerce/eshop/order/service/OrderService.java
+++ src/main/java/com/ecommerce/eshop/order/service/OrderService.java
@@ -2,8 +2,13 @@ package com.ecommerce.eshop.order.service;
 
 import com.ecommerce.eshop.order.models.Order;
 import com.ecommerce.eshop.order.repositories.OrderRepository;
+import com.ecommerce.eshop.product.models.Product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +17,18 @@ public class OrderService {
 
     public Order save(Order order){
         return order;
+    }
+
+    public Optional<Order> getByID(Long id){
+        return orderRepository.findById(id);
+    }
+
+    public List<Order> getAll(){
+        return orderRepository.findAll();
+    }
+
+    public List<Order> getAllThatIncludesProduct(Product product){
+        return Collections.emptyList();
     }
 
 
---
+++ src/test/java/com/ecommerce/eshop/order/service/OrderServiceTest.java
@@ -0,0 +1,44 @@
+package com.ecommerce.eshop.order.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @Test
+    void shouldSaveOrderToDB() {
+    }
+
+    @Test
+    void shouldReturnOrderById() {
+    }
+
+    @Test
+    void shouldReturnAllOrders() {
+    }
+
+    @Test
+    void shouldReturnListOfAllOrdersThatIncludesProduct() {
+    }
+
+    @Test
+    void shouldReturnListOfAllOrdersSortedByNewest() {
+
+    }
+
+    @Test
+    void shouldReturnListOfOrderBeforeGivenDate(){
+
+    }
+
+    @Test
+    void shouldReturnListOfOrderAfterGivenDate(){
+
+    }
+
+    @Test
+    void shouldReturnListOfOrderBetweenGivenDates(){
+
+    }
+}
