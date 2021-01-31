622913418d21179b2eecfc864ad23b6fee45e425
StanislawNagorski <stanislaw.nagorski@gmail.com>
2021-01-31 15:21:41 +0100

    security mapping for orders and products

--- src/main/java/com/ecommerce/eshop/utils/security/SecurityConfig.java
+++ src/main/java/com/ecommerce/eshop/utils/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.ecommerce.eshop.utils.security;
 
-import com.ecommerce.eshop.user.UserRole;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -23,15 +22,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("admin")
                 .roles(SELLER.name())
                 .build();
-        return new InMemoryUserDetailsManager(userDetails);
+
+        UserDetails customer = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles(CUSTOMER.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails,customer);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/products").permitAll()
+                .antMatchers(HttpMethod.GET, "/products").hasRole(CUSTOMER.name())
                 .antMatchers(HttpMethod.POST, "/products").hasRole(SELLER.name())
+                .antMatchers(HttpMethod.PUT,"/products").hasRole(SELLER.name())
                 .antMatchers(HttpMethod.DELETE, "/products").hasRole(SELLER.name())
+                 //TODO: osobne mapowanie dla customera(widzi swoje) i sprzedawcy(widzi wszystkie)
+                .antMatchers(HttpMethod.GET, "/orders").hasAnyRole(SELLER.name(), CUSTOMER.name())
+                .antMatchers(HttpMethod.POST, "/orders").hasRole(CUSTOMER.name())
+                .antMatchers(HttpMethod.PUT,"/orders").hasRole(ADMIN.name())
+                .antMatchers(HttpMethod.DELETE, "/orders").hasRole(ADMIN.name())
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll()
                 .and()
                 .csrf().disable();
     }
